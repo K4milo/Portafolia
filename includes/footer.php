47d59f9c6a2184369a45c5ab57c09c491c65d256
Camilo Vanegas <cvanegas@studiocom.ad>
2017-05-22 18:02:05 -0500

    ajustes de color txt

--- includes/footer.php
+++ includes/footer.php
@@ -10,10 +10,6 @@
 		    );
 		?>
     </div><!-- /.navbar-collapse -->
- 	<a href="/terms" class="terms">TERMS OF USE</a>
-    <div class="site-sub-footer">
-      <p>Portafolia Studio is a trade name of Portafolia Ltd. | Registered in England and Wales as a Limited Liability Company: 08386313 | Registered Office: 26 Stalbridge Flats, Lumley Street, W1K 6TA <br>  <?php echo date('Y'); ?> | PORTAFOLIA (All rights reserved) </p>
-    </div>
 
     <?php
         if(ICL_LANGUAGE_CODE=='en'){
@@ -26,7 +22,7 @@
         }
         if(ICL_LANGUAGE_CODE=='es'){
          ?>
-         <a href="/terms" class="terms">Términos de uso</a>
+         <a href="/terminos" class="terms">Términos de uso</a>
 		<div class="site-sub-footer">
 		  <p>Portafolia Studio es un nombre comercial de Portafolia Ltd. | Registrado en Inglaterra y Gales como una compañía de responsabilidad limitada: 08386313 | Domicilio social: 26 Stalbridge Flats, Lumley Street, W1K 6TA <br>  <?php echo date('Y'); ?> | PORTAFOLIA (Todos los derechos registrados) </p>
 		</div>
